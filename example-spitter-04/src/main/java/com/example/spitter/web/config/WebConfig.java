71a2b674bcca911103df214f8d74e797a60cfb5f
chenlxspace <clongxiang@163.com>
2017-03-21 09:39:24 +0800

    add

---
+++ example-spitter-04/src/main/java/com/example/spitter/web/config/WebConfig.java
@@ -0,0 +1,81 @@
+package com.example.spitter.web.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+/**
+ *
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.example.spitter.web.controller")
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    /**
+     * 参考Thymeleaf官方网站文档
+     * @return
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
+    }
+
+    /**
+     * 参考Thymeleaf官方网站文档
+     * @return
+     */
+    @Bean
+    public TemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setEnableSpringELCompiler(true);
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+    /**
+     * 参考Thymeleaf官方网站文档
+     * @return
+     */
+    private ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("/WEB-INF/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        return resolver;
+    }
+
+    /**
+     * 使用Servlet处理静态资源
+     *
+     * @param configurer
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
---
+++ example-spitter-04/src/main/java/com/example/spitter/web/controller/SpittleController.java
@@ -0,0 +1,56 @@
+package com.example.spitter.web.controller;
+
+import com.example.spitter.entity.Spittle;
+import com.example.spitter.entity.SpittleForm;
+import com.example.spitter.repository.SpittleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Richard on 2017/3/20.
+ */
+@Controller
+public class SpittleController {
+
+    private SpittleRepository spittleRepository;
+
+    /**
+     * 构造器注入
+     * @param spittleRepository
+     */
+    @Autowired
+    public SpittleController(SpittleRepository spittleRepository) {
+        this.spittleRepository = spittleRepository;
+    }
+
+    @RequestMapping(value = "/spittles",method = RequestMethod.GET)
+    public String spittles(@RequestParam(value = "max", defaultValue = "50") long max,
+                                  @RequestParam(value = "count", defaultValue = "30") int count,
+                                  Model model) {
+        List<Spittle> spittles = spittleRepository.findSpittles(max, count);
+        model.addAttribute("spittleList", spittles);
+        return "spittles";
+    }
+
+    @RequestMapping(value = "/{spittleId}", method = RequestMethod.GET)
+    public String spittle(@PathVariable("spittleId") long spittleId, Model model) {
+        model.addAttribute("spittle",spittleRepository.findOne(spittleId));
+        return "spittle";
+    }
+
+
+    @RequestMapping(value="/add", method = RequestMethod.POST)
+    public String saveSpittle(SpittleForm form, Model model) throws Exception {
+        spittleRepository.save(new Spittle(null, form.getMessage(), new Date(),
+                form.getLongitude(), form.getLatitude()));
+        return "redirect:/spittles";
+    }
+}
